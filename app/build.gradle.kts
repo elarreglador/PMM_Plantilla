b19987e19245416bd06cb197ef01ded3cda443e6
elarreglador <elarreglador@protonmail.com>
2024-11-18 06:53:56 +0100

    🈯 Modificado gradle: Version del compilador STK 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.elarreglador.pmm_parcial1"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.elarreglador.pmm_parcial1"
